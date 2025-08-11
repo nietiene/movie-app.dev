0e55d4cf552377fc63389323934efc0550ae05f3
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 16:04:34 -0700

    first commit of movie app

--- frontend/src/pages/movieDetail.jsx
+++ frontend/src/pages/movieDetail.jsx
@@ -1,2 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios"
+
+export default function MovieDetail() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+
+    const [movie, setMovie] = useState(null);
+    const [videoKey, setVideoKey] = useState(null);
+}

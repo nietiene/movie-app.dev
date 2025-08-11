630a0e9fc9f158c28358ba10c19d5c5dd612998a
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 15:10:47 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,10 @@ import axios from "axios";
 import cors from "cors";
 
 const app = express();
+const PORT = 5000;
+
+app.use(cors())
+
+const TMDB_API_KEY = "37b186e022267bd499bb77313a4cd229";
+
+// Fetch all popular movies from TMDB and forward to frontend

fb3ee05be4dacc96f8b7502741f51f814c84dec8
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 15:41:30 -0700

    first commit of movie app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
     <div className="max-w-7xl mx-auto p-6">
       <h1 className="text-4xl font-bold mb-8 text-center">Popular Movies</h1>
 
-      <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6">
         {movies.map(movie => (
           <div key={movie.id}>
             <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt={movie.title}

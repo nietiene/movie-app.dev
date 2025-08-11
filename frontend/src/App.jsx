27cf7fbd0d92e07f213ee722d4a763a69f4dbf45
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 15:25:18 -0700

    first commit of movie app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 function App() {
-  const [count, setCount] = useState(0)
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+      axios.get("ttp://localhost:5000/api/movies/popular")
+      .then(res => setMovies(res.data.results))
+      .catch(err => console.error(err));
+    }, [])
 
   return (
-    <>
+
+    <div className="max-w-7xl mx-auto p-6">
+      <h1>Popular Movies</h1>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
+
   )
 }
 

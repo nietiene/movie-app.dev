908d557163a136f754d624ae3141281f909e3f1e
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 13:48:35 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,6 @@ import Movie from "./models/movie.js"
 import { scrapeCategory } from "./scraper.js"
 
 const app = express()
+const PORT = 5000;
+
+mongoose.connect("mongodb+srv://<db_username>:<db_password>@etiene.jjrlz2m.mongodb.net/?retryWrites=true&w=majority&appName=Etiene")

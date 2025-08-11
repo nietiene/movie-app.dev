437b16bab63fd14933e28e7501a0c73b1f7eb51c
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 16:01:48 -0700

    first commit of movie app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import React, from "react"
+import React from "react"
 import axios from "axios"
-import 
+import Movie from "./pages/movie"
+import MovieDetail from "./pages/MovieDetail"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 function App() {
-
   return (
-
+   <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Movie />} />
+      <Route path="/" element={<Movie />} />
+     </Routes>
+   </BrowserRouter>
   )
 }
 

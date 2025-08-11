3dd279cf73411157c8553ae313397cce4d1734f2
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 16:33:13 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -11,15 +11,16 @@ const TMDB_API_KEY = "37b186e022267bd499bb77313a4cd229";
 
 // Fetch all popular movies from TMDB and forward to frontend
 
-app.get('/api/movies/popular', async (req, res) => {
+app.get('/api/movies', async (req, res) => {
+    const { category, search } = req.query;
     try {
-        const response = await axios.get(`https://api.themoviedb.org/3/movie/popular`, {
-            params: {
-                api_key: TMDB_API_KEY,
-                language: "en-US",
-                page: 1
-            }
-        });
+        let url, params;
+        
+        if (search) {
+            url = `https://api.themoviedb.org/3/movie/`
+            params = { api_key: TMDB_API_KEY, query: search, language: "en-US", page: 1};
+        }
+
         res.json(response.data);
     } catch (error) { 
         res.status(500).json({ error: "Failed to fetch movies" });

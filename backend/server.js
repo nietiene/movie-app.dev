f77be80d4ca1fc187f2594252bfccfe492a92ca2
nietiene <niyomugaboetiene53@gmail.com>
2025-08-11 13:54:25 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,11 @@ import { scrapeCategory } from "./scraper.js"
 const app = express()
 const PORT = 5000;
 
-mongoose.connect("mongodb+srv://mongoself:factorise@etiene.jjrlz2m.mongodb.net/movies")
+mongoose.connect("mongodb+srv://mongoself:factorise@etiene.jjrlz2m.mongodb.net/movies", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Mongo DB connected"))
+.catch(err => console.error("MongoDB connection Error", err))
+
+
+app.listen(PORT, () => console.log(`Server running http://localhost:${PORT}`))

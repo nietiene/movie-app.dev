fd6d2bc4965d7207e93d983978ff5d2f5e57f27f
nietiene <niyomugaboetiene53@gmail.com>
2025-08-12 15:24:28 -0700

    first commit of movie app

--- frontend/src/pages/movieDetail.jsx
+++ frontend/src/pages/movieDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios"
+import { FaArrowLeft, FaPlay, FaImdb, FaYoutube, FaSearch} from "react-icons/fa"
 
 export default function MovieDetail() {
     const { id } = useParams();

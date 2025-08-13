ff457b51d96733fd0cdfe28875c4c9719e501b6e
nietiene <niyomugaboetiene53@gmail.com>
2025-08-13 10:03:19 -0700

    first commit of movie app

--- frontend/src/pages/movie.jsx
+++ frontend/src/pages/movie.jsx
@@ -40,7 +40,7 @@ const Movie = () => {
     setSearch(e.target.value);
   };
 
-  const moviesPerPage = 50;
+  const moviesPerPage = 10;
   const paginatedMovies = movies.slice(page * moviesPerPage, (page + 1) * moviesPerPage);
   const totalPages = Math.ceil(movies.length / moviesPerPage);
 

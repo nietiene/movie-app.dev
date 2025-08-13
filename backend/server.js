f95376b66f540e29bb4db5124f4dad7745869edf
nietiene <niyomugaboetiene53@gmail.com>
2025-08-13 07:49:45 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: "https://netmoviess.vercel.app",
+    origin:  process.env.FRONTEND_API,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }))

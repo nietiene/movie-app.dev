ddece5843c0b7be3e2e085823df89a5ff987add8
nietiene <niyomugaboetiene53@gmail.com>
2025-08-12 17:43:38 -0700

    first commit of movie app

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    
-    origin:  "https://netmovies-theta.vercel.app/",
+    origin:  "https://netmovies-theta.vercel.app",
 }))
 
 const TMDB_API_KEY = process.env.TMDB_API_KEY;
